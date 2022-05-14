f2a02804ab7ab1954375a1632280bd2047b81735
gabrielteixeira-0814 <gabrielteixeira0814@gmail.com>
2022-05-13 22:21:36 -0300

    Iniciando a API

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ProductFactory extends Factory
+{
+
+
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'price' => $this->faker->randomFloat(2,0,8),
+            'description' => $this->faker->text()
+        ];
+    }
+}
--- database/migrations/2022_05_13_235013_create_table_products.php
+++ database/migrations/2022_05_13_235013_create_table_products.php
@@ -14,7 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name');
+            $table->float('price');
+            $table->text('description');
             $table->timestamps();
         });
     }
